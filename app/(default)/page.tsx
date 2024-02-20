6dff13b45219369fc1e6a875c868adfb5a6bd977
Wahed Hemati <w.hemati@googlemail.com>
2024-02-20 23:13:14 +0100

    added imprint

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Appcoholic - Home',
+  description: 'Welcome to Appcoholic. We are a team of developers and designers who love to create beautiful and functional apps. We are passionate about our work and we love to share our knowledge with the world. '
 }
 
 import Hero from '@/components/hero'
